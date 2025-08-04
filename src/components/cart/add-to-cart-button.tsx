a5601d3f2d0d5fa6e9cab8beec7279ffdee338e5
Victor Maoya <victormaoya@gmail.com>
2025-08-04 17:41:03 +0300

    before Co-Pilot

--- src/components/cart/add-to-cart-button.tsx
+++ src/components/cart/add-to-cart-button.tsx
@@ -11,12 +11,15 @@ interface AddToCartButtonProps {
 }
 
 export function AddToCartButton({ product }: AddToCartButtonProps) {
-  const addItem = useCart(state => state.addItem)
+  const addItem = useCart((state) => state.addItem)
 
   return (
     <Button
-      className="mt-8"
-      onClick={() => addItem(product)}
+      className='mt-8' 
+      onClick={() => {
+        console.log('clicked add to cart', product)
+        addItem(product)
+      }}
     >
       Add to Cart
     </Button>
